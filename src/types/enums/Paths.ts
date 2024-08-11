2caa9d9eb28ff4fbae88535f96aeb33883d69295
Ahmed Salah <ahmedsalah.abdelhak@gmail.com>
2024-08-11 04:23:34 +0300

    update route paths

--- src/types/enums/Paths.ts
+++ src/types/enums/Paths.ts
@@ -1,7 +1,7 @@
 import { PathSegments } from "./PathSegments";
 
 export enum Paths {
-  SHARED_CAMERAS = "/shared-cameras",
+  SHARED_CAMERAS = "/",
   SHARED_CAMERA = `/shared-camera/:${PathSegments.SHARED_CAMERA_ID}`,
   RECORDS = `/records/:${PathSegments.SHARED_CAMERA_ID}`,
 }
